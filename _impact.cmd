e622999d77592c699af7a3b5967c0ab95c5bc976
Alexander Gonzalez <agonzalez@stsg.com.co>
2018-09-15 21:38:40 -0500

    impact log
    se arreglan unos archivos para que el impact funcione correctamente.

--- _impact.cmd
+++ _impact.cmd
@@ -1,18 +1,12 @@
 setMode -bs
-setMode -ss
-setMode -sm
-setMode -hw140
-setMode -spi
-setMode -acecf
-setMode -acempm
-setMode -pff
-setMode -bs
 setMode -bs
 setMode -bs
 setMode -bs
 setCable -port auto
-addDevice -p 1 -file "/home/alex-pc/Documentos/ProyectosAlex/ElJuegoVida/VGA-FPGA/VGAT.bit"
-setCable -port auto
+Identify -inferir 
+identifyMPM 
+assignFile -p 1 -file "/home/alex-pc/Documentos/ProyectosAlex/ElJuegoVida/VGA-FPGA/VGAT.bit"
+Program -p 1 
 setMode -bs
 setMode -bs
 setMode -ss
@@ -26,6 +20,7 @@ setMode -bs
 saveProjectFile -file "/home/alex-pc/Documentos/ProyectosAlex/ElJuegoVida/VGA-FPGA//auto_project.ipf"
 setMode -bs
 setMode -bs
+deleteDevice -position 1
 deleteDevice -position 1
 setMode -bs
 setMode -ss
